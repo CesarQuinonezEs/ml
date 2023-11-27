ef5ac636554ab42b9a56fff0af37e533c4e7f982
CesarQuinonezEs <quinonez.cesar@uabc.edu.mx>
2023-11-27 09:01:21 -0800

    sudoku realizado

--- sodoku.py
+++ sodoku.py
@@ -1,76 +1,177 @@
-import numpy as np
+import random as rndm
+import time
 
-def inicializar_tablero():
-    """
-    Inicializa un tablero de Sudoku con valores iniciales.
-    """
-    tablero = np.zeros((9, 9), dtype=int)
-    # Aquí puedes ingresar los valores iniciales del Sudoku.
-    # Usa 0 para indicar celdas vacías.
-    return tablero
+def make_gene(initial=None):
+    if initial is None:
+        initial = [0] * 9
+    mapp = {}
+    gene = list(range(1, 10))
+    rndm.shuffle(gene)
+    for i in range(9):
+        mapp[gene[i]] = i
+    for i in range(9):
+        if initial[i] != 0 and gene[i] != initial[i]:
+            temp = gene[i], gene[mapp[initial[i]]]
+            gene[mapp[initial[i]]], gene[i] = temp
+            mapp[initial[i]], mapp[temp[0]] = i, mapp[initial[i]]
+    return gene
 
-def imprimir_tablero(tablero):
-    """
-    Imprime el tablero de Sudoku.
-    """
+
+def make_chromosome(initial=None):
+    if initial is None:
+        initial = [[0] * 9] * 9
+    chromosome = []
+    for i in range(9):
+        chromosome.append(make_gene(initial[i]))
+    return chromosome
+
+def make_population(count, initial=None):
+    if initial is None:
+        initial = [[0] * 9] * 9
+    population = []
+    for _ in range(count):
+        population.append(make_chromosome(initial))
+    return population
+
+
+def get_fitness(chromosome):
+    """Calculate the fitness of a chromosome (puzzle)."""
+    fitness = 0
+    for i in range(9): # For each column
+        seen = {}
+        for j in range(9): # Check each cell in the column
+            if chromosome[j][i] in seen:
+                seen[chromosome[j][i]] += 1
+            else:
+                seen[chromosome[j][i]] = 1
+        for key in seen: # Subtract fitness for repeated numbers
+            fitness -= (seen[key] - 1)
+    for m in range(3): # For each 3x3 square
+        for n in range(3):
+            seen = {}
+            for i in range(3 * n, 3 * (n + 1)):  # Check cells in 3x3 square
+                for j in range(3 * m, 3 * (m + 1)):
+                    if chromosome[j][i] in seen:
+                        seen[chromosome[j][i]] += 1
+                    else:
+                        seen[chromosome[j][i]] = 1
+            for key in seen: # Subtract fitness for repeated numbers
+                fitness -= (seen[key] - 1)
+    return fitness
+
+ch = make_chromosome()
+print(get_fitness(ch))
+
+
+def pch(ch):
     for i in range(9):
         for j in range(9):
-            print(tablero[i][j], end=" ")
-        print()
+            print(ch[i][j], end=" ")
+        print("")
 
-def fitness(tablero):
-    """
-    Evalúa la calidad de una solución al Sudoku.
-    Devuelve la cantidad de números únicos en filas y columnas.
-    """
-    filas, columnas = 0, 0
+def crossover(ch1, ch2):
+    new_child_1 = []
+    new_child_2 = []
     for i in range(9):
-        filas += len(set(tablero[i, :]))
-        columnas += len(set(tablero[:, i]))
-    return filas + columnas
+        x = rndm.randint(0, 1)
+        if x == 1:
+            new_child_1.append(ch1[i])
+            new_child_2.append(ch2[i])
+        elif x == 0:
+            new_child_2.append(ch1[i])
+            new_child_1.append(ch2[i])
+    return new_child_1, new_child_2
 
-def mutacion(tablero):
-    """
-    Realiza una mutación en el tablero de Sudoku.
-    Intercambia dos números en una fila aleatoria.
-    """
-    fila = np.random.randint(9)
-    col1, col2 = np.random.choice(9, size=2, replace=False)
-    tablero[fila, col1], tablero[fila, col2] = tablero[fila, col2], tablero[fila, col1]
+def mutation(ch, pm, initial):
+    for i in range(9):
+        x = rndm.randint(0, 100)
+        if x < pm * 100:
+            ch[i] = make_gene(initial[i])
+    return ch
 
-def reproduccion(padre1, padre2):
-    """
-    Realiza la reproducción de dos tableros de Sudoku (cruce).
-    """
-    punto_cruce = np.random.randint(1, 8)
-    hijo = np.vstack((padre1[:punto_cruce, :], padre2[punto_cruce:, :]))
-    return hijo
+def read_puzzle(address):
+    puzzle = []
+    f = open(address, 'r')
+    for row in f:
+        temp = row.split()
+        puzzle.append([int(c) for c in temp])
+    return puzzle
 
-def algoritmo_genetico():
-    """
-    Implementa el algoritmo genético para resolver Sudoku.
-    """
-    tablero = inicializar_tablero()
-    iteraciones = 1000
-    tasa_mutacion = 0.1
+def r_get_mating_pool(population):
+    fitness_list = []
+    pool = []
+    for chromosome in population:
+        fitness = get_fitness(chromosome)
+        fitness_list.append((fitness, chromosome))
+    fitness_list.sort()
+    weight = list(range(1, len(fitness_list) + 1))
+    for _ in range(len(population)):
+        ch = rndm.choices(fitness_list, weight)[0]
+        pool.append(ch[1])
+    return pool
 
-    for _ in range(iteraciones):
-        fitness_values = [fitness(tablero) for _ in range(20)]
-        seleccionados = np.argsort(fitness_values)[:2]
+def w_get_mating_pool(population):
+    fitness_list = []
+    pool = []
+    for chromosome in population:
+        fitness = get_fitness(chromosome)
+        fitness_list.append((fitness, chromosome))
+    weight = [fit[0] - fitness_list[0][0] for fit in fitness_list]
+    for _ in range(len(population)):
+        ch = rndm.choices(fitness_list, weights=weight)[0]
+        pool.append(ch[1])
+    return pool
 
-        padre1 = tablero[seleccionados[0]]
-        padre2 = tablero[seleccionados[1]]
+def get_offsprings(population, initial, pm, pc):
+    new_pool = []
+    i = 0
+    while i < len(population):
+        ch1 = population[i]
+        ch2 = population[(i + 1) % len(population)]
+        x = rndm.randint(0, 100)
+        if x < pc * 100:
+            ch1, ch2 = crossover(ch1, ch2)
+        new_pool.append(mutation(ch1, pm, initial))
+        new_pool.append(mutation(ch2, pm, initial))
+        i += 2
+    return new_pool
 
-        hijo = reproduccion(padre1, padre2)
+# Population size
+POPULATION = 1000
 
-        if np.random.rand() < tasa_mutacion:
-            mutacion(hijo)
+# Number of generations
+REPETITION = 1000
 
-        tablero[seleccionados[0]] = hijo
+# Probability of mutation
+PM = 0.1
 
-    return tablero
+# Probability of crossover
+PC = 0.95
 
-if __name__ == "__main__":
-    solucion = algoritmo_genetico()
-    print("Tablero de Sudoku resuelto:")
-    imprimir_tablero(solucion)
+# Main genetic algorithm function
+def genetic_algorithm(initial_file):
+    initial = read_puzzle(initial_file)
+    population = make_population(POPULATION, initial)
+    for _ in range(REPETITION):
+        mating_pool = r_get_mating_pool(population)
+        rndm.shuffle(mating_pool)
+        population = get_offsprings(mating_pool, initial, PM, PC)
+        fit = [get_fitness(c) for c in population]
+        m = max(fit)
+        if m == 0:
+            return population
+    return population
+
+tic = time.time()
+r = genetic_algorithm("./Test2.txt")
+toc = time.time()
+print("time_taken: ", toc - tic)
+fit = [get_fitness(c) for c in r]
+m = max(fit)
+print(max(fit))
+
+# Print the chromosome with the highest fitness
+for c in r:
+    if get_fitness(c) == m:
+        pch(c)
+        break
